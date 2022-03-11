b361b582bad6ca23a5743ef69e588e34f942f622
Adam Howard <adam.howard.music@gmail.com>
2022-03-11 17:01:06 +0100

    Add type definitions to tests

--- test/TriviaCard.spec.ts
+++ test/TriviaCard.spec.ts
@@ -1,8 +1,11 @@
-import { mount } from '@vue/test-utils';
-import TriviaCard from '@/components/TriviaCard.vue';
-import TriviaCardPlaceholder from '~/components/TriviaCard/Placeholder.vue';
-import FilterButton from '~/components/Filter/Button.vue';
-import { initialiseTriviaQuestion } from '~/@types/trivia-question';
+import { mount, Wrapper } from "@vue/test-utils";
+import TriviaCard from "@/components/TriviaCard.vue";
+import TriviaCardPlaceholder from "~/components/TriviaCard/Placeholder.vue";
+import FilterButton from "~/components/Filter/Button.vue";
+import {
+  initialiseTriviaQuestion,
+  TriviaQuestion,
+} from "~/@types/trivia-question";
 
 // Because I wanted to take advantage of Nuxt's auto import of components
 // I had to do something a little 'hacky' feeling to get the tests to accept
@@ -10,53 +13,52 @@ import { initialiseTriviaQuestion } from '~/@types/trivia-question';
 // component. The solution I used, can be found here:
 // https://github.com/nuxt/components/issues/58#issuecomment-904230080
 
-describe('TriviaCard.vue', () => {
-  const testQuestion = initialiseTriviaQuestion({
+describe("TriviaCard.vue", () => {
+  const testQuestion: TriviaQuestion = initialiseTriviaQuestion({
     id: 1,
-    category: 'General Knowledge',
-    type: 'boolean',
-    difficulty: 'medium',
-    question: 'Cucumbers are usually more than 90% water.',
-    correct_answer: 'True',
-    incorrect_answers: ['False']
+    category: "General Knowledge",
+    type: "boolean",
+    difficulty: "medium",
+    question: "Cucumbers are usually more than 90% water.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
   });
 
-  test('Test that Trivia Card Rendors Boolean Question', () => {
-    const wrapper = mount(TriviaCard, {
+  test("Test that Trivia Card Rendors Boolean Question", () => {
+    const wrapper: Wrapper<TriviaCard, Element> = mount(TriviaCard, {
       propsData: {
-        question: testQuestion
-      }
+        question: testQuestion,
+      },
     });
     expect(wrapper.text()).toMatch(/True or False:/);
   });
 
-  test('Test that Trivia Card Flips When Clicked', async () => {
-    const wrapper = mount(TriviaCard, {
+  test("Test that Trivia Card Flips When Clicked", async () => {
+    const wrapper: Wrapper<TriviaCard, Element> = mount(TriviaCard, {
       propsData: {
-        question: testQuestion
-      }
+        question: testQuestion,
+      },
     });
-    const flipCard = wrapper.find('.flip-card');
-    await flipCard.trigger('click');
+
+    const flipCard = wrapper.find(".flip-card");
+    await flipCard.trigger("click");
     expect(wrapper.html()).toMatch(/flipped/);
-    await flipCard.trigger('click');
+    await flipCard.trigger("click");
     expect(wrapper.html()).not.toMatch(/flipped/);
   });
 });
 
-describe('TriviaCardPlaceholder.vue', () => {
-  test('Test that Trivia Card Placeholder Rendors', () => {
-    const wrapper = mount(TriviaCardPlaceholder);
+describe("TriviaCardPlaceholder.vue", () => {
+  test("Test that Trivia Card Placeholder Rendors", () => {
+    const wrapper: Wrapper<TriviaCardPlaceholder, Element> = mount(TriviaCardPlaceholder);
     expect(wrapper.vm).toBeTruthy();
   });
 });
 
-describe('FilterButton.vue', () => {
-  test('Test that the filter button renders the X icon when active', () => {
-    const wrapper = mount(FilterButton, {
-      propsData: {
-        active: true
-      }
+describe("FilterButton.vue", () => {
+  test("Test that the filter button renders the X icon when active", () => {
+    const wrapper: Wrapper<FilterButton, Element> = mount(FilterButton, {
+      propsData: { active: true },
     });
     expect(wrapper.html()).toMatch(
       /<svg[\w\W]*?>[\w\W]*<circle[\w\W]*?<\/circle>[\w\W]*<polygon[\w\W]*?><\/polygon>[\w\W]*?<\/svg>/
@@ -64,10 +66,8 @@ describe('FilterButton.vue', () => {
   });
 
   test("Test that the filter button doesn't render the X icon when inactive", () => {
-    const wrapper = mount(FilterButton, {
-      propsData: {
-        active: false
-      }
+    const wrapper: Wrapper<FilterButton, Element> = mount(FilterButton, {
+      propsData: { active: false },
     });
     expect(wrapper.html()).not.toMatch(
       /<svg[\w\W]*?>[\w\W]*<circle[\w\W]*?<\/circle>[\w\W]*<polygon[\w\W]*?><\/polygon>[\w\W]*?<\/svg>/
